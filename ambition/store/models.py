ed48802dc42108cce9dac9af144ed68a43076551
주현지 <hyjoo0909@naver.com>
2023-07-05 23:48:56 +0900

    fix : User 모델 수정

--- ambition/store/models.py
+++ ambition/store/models.py
@@ -3,5 +3,5 @@ from django.db import models
 # Create your models here.
 class User(models.Model):
     name = models.CharField(verbose_name='이름', max_length=16)
-    email= models.EmailField(verbose_name='이메일', max_length=64)
-    token= models.CharField(verbose_name='비밀번호', max_length=64)
+    email = models.EmailField(verbose_name='이메일', max_length=64)
+    token = models.CharField(verbose_name='비밀번호', max_length=64)
