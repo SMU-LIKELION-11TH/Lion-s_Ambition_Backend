8885510cfdd2e74754deac8f2c6ddf9df64e9998
김동주 <19310326+Hepheir@users.noreply.github.com>
2023-07-05 23:45:36 +0900

    feat: Category 모델 생성 (#2)

--- ambition/store/models.py
+++ ambition/store/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+
 class Order(models.Model):
     status = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
