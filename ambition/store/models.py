73758aa90be5da2e85e21482d0cbb20593984d5b
CastleBravo01 <96935944+CastleBravo01@users.noreply.github.com>
2023-07-05 23:42:16 +0900

    feat: Order 모델 생성 (#3)
    * feat: Order 모델 생성
    
    * refactor:Order 모델 수정 요청 사항 반영

--- ambition/store/models.py
+++ ambition/store/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Order(models.Model):
+    status = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
