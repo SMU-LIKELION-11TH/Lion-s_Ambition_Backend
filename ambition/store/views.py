a698a4eb7c45ea033315119eb1e1ec1caf42594b
주현지 <hyjoo0909@naver.com>
2023-07-08 12:57:48 +0900

    feat : UserCreateView 기능 구현
    사용자 회원가입

--- ambition/store/views.py
+++ ambition/store/views.py
@@ -10,18 +10,41 @@ from store.models import Category
 
 class UserCreateView(View):
     "/signup"
+    def post(self, request):
+        data = json.loads(request.body)
+        email = data['email']
 
     def post(self, request: HttpRequest) -> HttpResponse:
         """사용자/추가(회원가입)"""
         pass
+        if not re.match(r"[^@]+@[^@]+\.[^@]+", email):
+            return HttpResponse({'message': '이메일의 형식이 옳지 않습니다. 입력하신 내용을 다시 확인해주세요.'}, status=400)  # Invalid email format
 
+        verification_code = generate_verification_code()  # Generate a random verification code
+        send_verification_email(email, verification_code)  # Send the verification code
 
 class UserLoginView(View):
     "/login"
+        store.objects.create(
+            name=data['name'],
+            email=data['email'],
+            token=data['token'],
+            verification_code=verification_code,  # Save the verification code to the user model
+        )
+        return HttpResponse({'message': '회원가입 완료'}, status=201)  # Successful user create
 
     def post(self, request: HttpRequest) -> HttpResponse:
         """사용자/로그인"""
         pass
+    def get(self, request):  # save user data
+        User_data = User.objects.values()
+        return JsonResponse({'users': list(User_data)}, status=200)
+
+def generate_verification_code():
+    return str(random.randint(10000, 99999))  # random 5-digit verification code
+
+def send_verification_email(email, verification_code):
+    #  .....모르겠어요...
 
 
 class UserLogoutView(View):
