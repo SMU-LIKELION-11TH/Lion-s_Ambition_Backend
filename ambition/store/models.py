4bf6e59fcbf590fd0012d2c6c21e41286f83f9be
Hepheir <hepheir@gmail.com>
2023-07-10 02:17:33 +0900

    refactor!: User 모델의 비밀번호 속성 재설계

--- ambition/store/models.py
+++ ambition/store/models.py
@@ -2,9 +2,10 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    name = models.CharField(verbose_name='이름', max_length=16)
-    email = models.EmailField(verbose_name='이메일', max_length=64)
-    token = models.CharField(verbose_name='비밀번호', max_length=64)
+    name = models.CharField(max_length=16)
+    email = models.EmailField(max_length=64, unique=True)
+    password = models.CharField(max_length=64)
+    kakao_oauth_token = models.CharField(max_length=64)
 
 
 class EmailValidation(models.Model):
