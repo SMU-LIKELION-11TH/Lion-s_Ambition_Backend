a6af5097e95c00e1c9cca9610b963eddec9f75fc
Hepheir <hepheir@gmail.com>
2023-07-10 20:43:27 +0900

    refactor: View와 Model, DTO, Serializer 재설계

--- ambition/store/models.py
+++ ambition/store/models.py
@@ -1,21 +1,125 @@
+from __future__ import annotations
+
+import datetime
+import random
+
+from django.core.exceptions import ObjectDoesNotExist, ValidationError
+from django.core.mail import EmailMessage
 from django.db import models
 
+from store.dto import *
+
 # Create your models here.
+
+
+class EmailValidation(models.Model):
+    @classmethod
+    def create_or_update_from_dto(cls, dto: EmailValidationRequestDTO) -> EmailValidation:
+        entity = cls.objects.get_or_create(email=dto.email)[0]
+        entity.codes = cls.generate_codes()
+        entity.created_at = datetime.datetime.now()
+        entity.save()
+        return entity
+
+    @classmethod
+    def validate_from_dto(cls, dto: UserRegistrationDTO) -> bool:
+        try:
+            entity = cls.objects.get(email=dto.email)
+            if entity.codes != dto.validation:
+                raise AssertionError()
+            entity.delete()
+            return True
+        except AssertionError:
+            pass
+        except ObjectDoesNotExist:
+            pass
+        finally:
+            return False
+
+    @classmethod
+    def generate_codes(cls, length=5) -> str:
+        """알파벳 n자리의 인증 코드를 임의로 생성합니다."""
+        codes = []
+        for i in range(length):
+            codes.append(chr(random.randint(ord('A'), ord('Z'))))
+        return ''.join(codes)
+
+    email = models.EmailField(primary_key=True, max_length=64)
+    codes = models.EmailField(max_length=6)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def send_email(self):
+        email = EmailMessage()
+        email.subject = "야심작 이메일 인증번호"
+        email.body = f"인증번호는 {self.codes}입니다."
+        email.to = [self.email]
+        email.send()
+
+
 class User(models.Model):
+    @classmethod
+    def create_from_dto(cls, dto: UserRegistrationDTO) -> User:
+        entity = User()
+        entity.name = dto.name
+        entity.email = dto.email
+        entity.password = dto.password
+        entity.kakao_oauth_token = None
+        entity.save()
+        return entity
+
     name = models.CharField(max_length=16)
     email = models.EmailField(max_length=64, unique=True)
     password = models.CharField(max_length=64)
     kakao_oauth_token = models.CharField(max_length=64, null=True)
 
 
-class EmailValidation(models.Model):
-    email = models.EmailField(primary_key=True, max_length=64)
-    codes = models.EmailField(max_length=6)
-    created_at = models.DateTimeField(auto_now_add=True)
-
-
 class Category(models.Model):
     name = models.CharField(max_length=100)
+
+
+class Product(models.Model):
+    @classmethod
+    def create_from_dto(cls, dto: ProductCreationDTO) -> Product:
+        entity = Product()
+        entity.category = Category.objects.get(pk=int(dto.category_id))
+        entity.name = dto.name
+        entity.primary_image_url = dto.image_url
+        entity.regular_price = dto.price
+        entity.is_soldout = dto.soldout
+        entity.save()
+        return entity
+
+    @classmethod
+    def query_from_dto(cls, dto: ProductQueryDTO) -> models.query.QuerySet[Product]:
+        kwargs = {}
+        if dto.category_id is not None:
+            kwargs['category'] = Category.objects.get(pk=dto.category_id)
+        if dto.soldout is not None:
+            kwargs['is_soldout'] = dto.soldout
+        return cls.objects.filter(**kwargs)
+
+    @classmethod
+    def update_from_dto(cls, pk: int, dto: ProductModificationDTO) -> Product:
+        entity = Product.objects.get(pk=pk)
+        if dto.category_id is not None:
+            entity.category = Category.objects.get(pk=int(dto.category_id))
+        if dto.name is not None:
+            entity.name = dto.name
+        if dto.image_url is not None:
+            entity.primary_image_url = dto.image_url
+        if dto.price is not None:
+            entity.regular_price = dto.price
+        if dto.soldout is not None:
+            entity.is_soldout = dto.soldout
+        entity.save()
+        return entity
+
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100, unique=True,
+                            blank=False, null=False)
+    primary_image_url = models.CharField(max_length=200, null=False)
+    regular_price = models.IntegerField(null=False)
+    is_soldout = models.BooleanField()
 
 
 class OrderStatus(models.Model):
@@ -23,17 +127,46 @@ class OrderStatus(models.Model):
 
 
 class Order(models.Model):
+    @classmethod
+    def create_from_dto(cls, dto: OrderCreationDTO) -> Order:
+        entities = []
+        order = Order()
+        order.status = OrderStatus.objects.get(pk=1)
+        entities.append(order)
+        for item in dto.items:
+            order_item = OrderItem()
+            order_item.order = order
+            order_item.product = Product.objects.get(pk=item.product_id)
+            order_item.unit_price = order_item.product.regular_price
+            order_item.quantity = item.quantity
+            entities.append(order_item)
+        for entity in entities:
+            entity.save()
+        return order
+
+    @classmethod
+    def query_from_dto(cls, dto: OrderQueryDTO) -> models.query.QuerySet[Order]:
+        kwargs = {}
+        if dto.status is not None:
+            kwargs['status'] = OrderStatus.objects.get(pk=dto.status)
+        if dto.create_year is not None:
+            kwargs['created_at__year'] = dto.create_year
+        if dto.create_month is not None:
+            # TODO: 월별 조회 기능 구현
+            raise NotImplementedError('월별 조회 기능은 아직 지원되지 않습니다.')
+        return cls.objects.filter(**kwargs)
+
+    @classmethod
+    def update_from_dto(cls, pk: int, dto: OrderModificationDTO) -> Order:
+        entity = cls.objects.get(pk=pk)
+        entity.status = OrderStatus.objects.get(pk=dto.status)
+        entity.updated_at = datetime.datetime.now()
+        entity.save()
+        return entity
+
     status = models.ForeignKey(OrderStatus, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
-
-class Product(models.Model):
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    name = models.CharField(max_length=100, unique=True, blank=False, null=False)
-    primary_image_url = models.CharField(max_length=200, null=False)
-    regular_price = models.IntegerField(null=False)
-    is_soldout = models.BooleanField()
 
 
 class OrderItem(models.Model):
--- ambition/store/views.py
+++ ambition/store/views.py
@@ -5,12 +5,13 @@ import random
 from http import HTTPStatus
 from typing import *
 
-from django.core.exceptions import ObjectDoesNotExist
+from django.core.exceptions import ObjectDoesNotExist, ValidationError
 from django.core.mail import EmailMessage
 from django.db import IntegrityError
 from django.http import HttpRequest, HttpResponse, JsonResponse, QueryDict
 from django.views.generic import View
 
+from store.dto import *
 from store.models import *
 from store.serializers import *
 
@@ -18,10 +19,6 @@ from store.serializers import *
 
 
 class EmailValidationView(View):
-    @dataclasses.dataclass
-    class RequestDTO:
-        email: str
-
     def post(self, request: HttpRequest) -> HttpResponse:
         """이메일 인증 기능을 수행합니다.
 
@@ -29,91 +26,35 @@ class EmailValidationView(View):
         이메일 주소가 포함 되어있지 않다면 400 BadRequest 응답코드를 반환합니다.
         """
         try:
-            dto = self.parse_request_body(request)
-            code = self.generate_random_code(5)
-            self.send_email(dto, code)
-            self.save_entity(dto, code)
+            dto = EmailValidationRequestDTO.from_request(request)
+            entity = EmailValidation.create_or_update_from_dto(dto)
+            entity.send_email()
             return HttpResponse(status=HTTPStatus.OK)
         except (KeyError, ValueError):
             return HttpResponse(status=HTTPStatus.BAD_REQUEST)
-
-    def parse_request_body(self, request: HttpRequest) -> RequestDTO:
-        # TODO: 이메일 주소 형식이 올바른지 검사하기.
-        data = QueryDict(request.body)
-        return EmailValidationView.RequestDTO(email=data['email'])
-
-    def generate_random_code(self, length: int) -> str:
-        """임시로 발급할 알파벳 n자리의 코드를 임의로 생성합니다."""
-        codes = []
-        for i in range(length):
-            codes.append(chr(random.randint(ord('A'), ord('Z'))))
-        return ''.join(codes)
-
-    def send_email(self, dto: RequestDTO, code: str) -> EmailMessage:
-        email = EmailMessage()
-        email.subject = "야심작 이메일 인증번호"
-        email.body = f"인증번호는 {code}입니다."
-        email.to = [dto.email]
-        email.send()
-
-    def save_entity(self, dto: RequestDTO, code: str) -> EmailValidation:
-        entity, is_created = EmailValidation.objects.get_or_create(email=dto.email)
-        entity.codes = code
-        entity.created_at = datetime.datetime.now()
-        entity.save()
 
 
 class UserCreateView(View):
     "/signup"
 
-    @dataclasses.dataclass
-    class RequestDTO:
-        email: str
-        password: str
-        name: str
-        validation_code: str
-
     def post(self, request: HttpRequest) -> HttpResponse:
         """사용자/추가(회원가입)"""
         try:
-            dto = self.parse_request_body(request)
-            self.check_email_validation_code(dto)
-            user = self.create_user(dto)
+            dto = UserRegistrationDTO.from_request(request)
+            is_valid = EmailValidation.validate_from_dto(dto)
+            if not is_valid:
+                raise ValidationError('이메일 인증 코드가 올바르지 않습니다.')
+            user = User.create_from_dto(dto)
             return JsonResponse(status=HTTPStatus.CREATED, data={
                 "message": "회원가입에 성공하였습니다.",
-                "data": {
-                    "user": serializeUser(user)
-                }
+                "data": { "user": serializeUser(user) },
             })
         except ValueError:
             return HttpResponse(status=HTTPStatus.BAD_REQUEST)
-        except (AssertionError, ObjectDoesNotExist):
+        except (AssertionError, ObjectDoesNotExist, ValidationError):
             return HttpResponse(status=HTTPStatus.UNAUTHORIZED)
         except IntegrityError:
             return HttpResponse(status=HTTPStatus.CONFLICT)
-
-    def parse_request_body(self, request: HttpRequest) -> RequestDTO:
-        data = QueryDict(request.body)
-        return UserCreateView.RequestDTO(
-            email=data['email'],
-            password=data['password'],
-            name=data['name'],
-            validation_code=data['validation-code'],
-        )
-
-    def check_email_validation_code(self, dto: RequestDTO):
-        entity = EmailValidation.objects.get(email=dto.email)
-        if entity.codes != dto.validation_code:
-            raise AssertionError()
-
-    def create_user(self, dto: RequestDTO) -> User:
-        entity = User()
-        entity.name = dto.name
-        entity.email = dto.email
-        entity.password = dto.password
-        entity.kakao_oauth_token = None
-        entity.save()
-        return entity
 
 
 class UserLoginView(View):
@@ -127,8 +68,8 @@ class UserLoginView(View):
     def post(self, request: HttpRequest) -> HttpResponse:
         """사용자/로그인"""
         try:
-            dto = self.parse_request_body(request)
             # TODO: 로그인 기능 구현
+            dto = self.parse_request_body(request)
             return JsonResponse(status=HTTPStatus.OK, data={})
         except ValueError:
             return HttpResponse(status=HTTPStatus.BAD_REQUEST)
@@ -152,102 +93,33 @@ class UserLogoutView(View):
 class OrderView(View):
     "/order"
 
-    @dataclasses.dataclass
-    class QueryDTO:
-        created_year: Optional[int]
-        created_month: Optional[int]
-        status: Optional[int]
-
-    @dataclasses.dataclass
-    class OrderItemDTO:
-        product_id: int
-        quantity: int
-
     def get(self, request: HttpRequest) -> HttpResponse:
         """주문/조회/여러 항목 조회"""
         try:
-            self._validate_user_logged_in(request)
-            dto = self._parse_query_dto(request)
-            order_entities = self._query_order_entities(dto)
+            # TODO: 로그인 기능 구현
+            dto = OrderQueryDTO.from_request(request)
+            entities = Order.query_from_dto(dto)
             return JsonResponse(status=HTTPStatus.OK, data={
                 "data": {
-                    "orders": list(map(serializeOrder, order_entities)),
+                    "orders": list(map(serializeOrder, entities)),
                 }
             })
         except (ValueError, ObjectDoesNotExist):
             return HttpResponse(status=HTTPStatus.BAD_REQUEST)
 
-    def _parse_query_dto(self, request: HttpRequest) -> QueryDTO:
-        data = request.GET
-        return OrderView.QueryDTO(
-            created_year=self._parse_optional_int(data.get('create-year')),
-            created_month=self._parse_optional_int(data.get('create-month')),
-            status=self._parse_optional_int(data.get('status')),
-        )
-
-    def _parse_optional_int(self, optional_int_like: str | None) -> int | None:
-        if optional_int_like is not None:
-            return int(optional_int_like)
-        return optional_int_like
-
-    def _query_order_entities(self, dto: QueryDTO) -> List[Order]:
-        kwargs = {}
-        retval = []
-        if dto.status is not None:
-            kwargs['status'] = OrderStatus.objects.get(pk=dto.status)
-        for entity in Order.objects.filter(**kwargs):
-            if (dto.created_year is not None) and (entity.created_at.year != dto.created_year):
-                continue
-            if (dto.created_month is not None) and (entity.created_at.month != dto.created_month):
-                continue
-            retval.append(entity)
-        return retval
-
     def post(self, request: HttpRequest) -> HttpResponse:
         """주문/생성"""
         try:
-            self._validate_user_logged_in(request)
-            orderitem_dto_list = self._parse_orderitem_dto_list(request)
-            order_entity = self._save_order(orderitem_dto_list)
+            # TODO: 로그인 기능 구현
+            dto = OrderCreationDTO.from_request(request)
+            entity = Order.create_from_dto(dto)
             return JsonResponse(status=HTTPStatus.CREATED, data={
                 "data": {
-                    "order": serializeOrder(order_entity),
+                    "order": serializeOrder(entity),
                 }
             })
-        except ValueError:
+        except (KeyError, ValueError):
             return HttpResponse(status=HTTPStatus.BAD_REQUEST)
-
-    def _parse_orderitem_dto_list(self, request: HttpRequest) -> List[OrderItemDTO]:
-        data = QueryDict(request.body)
-        retval = []
-        for item in json.loads(data['items']):
-            orderitem = OrderView.OrderItemDTO(
-                product_id=int(item['product-id']),
-                quantity=int(item['quantity']),
-            )
-            retval.append(orderitem)
-        return retval
-
-    def _save_order(self, dto_list: List[OrderItemDTO]) -> Order:
-        entities = []
-        order = Order()
-        order.status = OrderStatus.objects.get(pk=1)
-        entities.append(order)
-        for dto in dto_list:
-            product = Product.objects.get(pk=dto.product_id)
-            orderitem = OrderItem()
-            orderitem.product = product
-            orderitem.order = order
-            orderitem.unit_price = product.regular_price
-            orderitem.quantity = dto.quantity
-            entities.append(orderitem)
-        for entity in entities:
-            entity.save()
-        return order
-
-    def _validate_user_logged_in(self, request: HttpRequest):
-        # TODO: 기능 구현
-        pass
 
 
 class OrderIdView(View):
@@ -260,11 +132,10 @@ class OrderIdView(View):
     def get(self, request: HttpRequest, order_id: int) -> HttpResponse:
         """주문/조회/단일 항목 조회"""
         try:
-            self._validate_user_logged_in(request)
-            order = Order.objects.get(pk=order_id)
+            entity = Order.objects.get(pk=order_id)
             return JsonResponse(status=HTTPStatus.OK, data={
                 "data": {
-                    "order": serializeOrder(order)
+                    "order": serializeOrder(entity)
                 }
             })
         except ObjectDoesNotExist:
@@ -273,30 +144,18 @@ class OrderIdView(View):
     def patch(self, request: HttpRequest, order_id: int) -> HttpResponse:
         """주문/수정"""
         try:
-            self._validate_user_logged_in(request)
-            dto = self._parse_order_update_dto(request)
-            order = Order.objects.get(pk=order_id)
-            order.status = OrderStatus.objects.get(pk=dto.status)
-            order.save()
+            # TODO: 로그인 기능 구현
+            dto = OrderModificationDTO.from_request(request)
+            entity = Order.update_from_dto(order_id, dto)
             return JsonResponse(status=HTTPStatus.OK, data={
                 "data": {
-                    "order": serializeOrder(order)
+                    "order": serializeOrder(entity)
                 }
             })
-        except ValueError:
+        except (KeyError, ValueError):
             return HttpResponse(status=HTTPStatus.BAD_REQUEST)
         except ObjectDoesNotExist:
             return HttpResponse(status=HTTPStatus.NOT_FOUND)
-
-    def _parse_order_update_dto(self, request: HttpRequest) -> OrderUpdateDTO:
-        data = QueryDict(request.body)
-        return OrderIdView.OrderUpdateDTO(
-            status=int(data['status']),
-        )
-
-    def _validate_user_logged_in(self, request: HttpRequest):
-        # TODO: 기능 구현
-        pass
 
 
 class ProductView(View):
@@ -304,55 +163,34 @@ class ProductView(View):
 
     def get(self, request: HttpRequest) -> HttpResponse:
         """상품/조회/여러 항목 조회"""
+        # TODO: pagination 기능 추가
         try:
-            data = request.GET
-            query_kwargs = {}
-            if 'category_id' in data:
-                query_kwargs['category'] = Category.objects.get(pk=int(data.get('category_id')))
-            if 'soldout' in data:
-                if isinstance(data.get('soldout'), str):
-                    query_kwargs['is_soldout'] = data.get('soldout') == 'true'
-                elif isinstance(data.get('soldout'), bool):
-                    query_kwargs['is_soldout'] = data.get('soldout')
-                else:
-                    raise ValueError()
-            # TODO: pagination 기능 추가
-            products = Product.objects.filter(**query_kwargs)
+            dto = ProductQueryDTO.from_request(request)
+            entities = Product.query_from_dto(dto)
             return JsonResponse(status=HTTPStatus.OK, data={
                 "data": {
-                    "products": list(map(serializeProduct, products)),
+                    "products": list(map(serializeProduct, entities)),
                 },
             })
         except ObjectDoesNotExist:
             return JsonResponse(status=HTTPStatus.BAD_REQUEST, data={"message": "Category not found"})
-        except ValueError:
+        except (KeyError, ValueError):
             return JsonResponse(status=HTTPStatus.BAD_REQUEST, data={})
 
     def post(self, request: HttpRequest) -> HttpResponse:
         """상품/추가"""
         # TODO: 사용자 인증 기능 추가
         try:
-            data = request.POST
-            product = Product()
-            product.category = Category.objects.get(pk=int(data.get('category_id')))
-            product.name = data.get('name')
-            product.primary_image_url = data.get('image_url')
-            product.regular_price = int(data.get('price'))
-            if isinstance(data.get('soldout'), str):
-                product.is_soldout = data.get('soldout') == 'true'
-            elif isinstance(data.get('soldout'), bool):
-                product.is_soldout = data.get('soldout')
-            else:
-                raise ValueError()
-            product.save()
+            dto = ProductCreationDTO.from_request(request)
+            entity = Product.create_from_dto(dto)
             return JsonResponse(status=HTTPStatus.CREATED, data={
                 "data": {
-                    "product": serializeProduct(product),
+                    "product": serializeProduct(entity),
                 },
             })
         except ObjectDoesNotExist:
             return JsonResponse(status=HTTPStatus.BAD_REQUEST, data={"message": "Category not found"})
-        except ValueError:
+        except (KeyError, ValueError):
             return JsonResponse(status=HTTPStatus.BAD_REQUEST, data={})
         except IntegrityError:
             return JsonResponse(status=HTTPStatus.CONFLICT, data={})
@@ -365,34 +203,16 @@ class ProductIdView(View):
         """상품/수정"""
         # TODO: 사용자 인증 기능 추가
         try:
-            data = QueryDict(request.body)
-            if not data:
-                raise ValueError()
-            product = Product.objects.get(pk=product_id)
-            if 'category_id' in data:
-                product.category = Category.objects.get(pk=int(data.get('category_id')))
-            if 'name' in data:
-                product.name = data.get('name')
-            if 'image_url' in data:
-                product.primary_image_url = data.get('image_url')
-            if 'price' in data:
-                product.regular_price = int(data.get('price'))
-            if 'soldout' in data:
-                if isinstance(data.get('soldout'), str):
-                    product.is_soldout = data.get('soldout') == 'true'
-                elif isinstance(data.get('soldout'), bool):
-                    product.is_soldout = data.get('soldout')
-                else:
-                    raise ValueError()
-            product.save()
+            dto = ProductModificationDTO.from_request(request)
+            entity = Product.update_from_dto(product_id, dto)
             return JsonResponse(status=HTTPStatus.OK, data={
                 "data": {
-                    "product": serializeProduct(product),
+                    "product": serializeProduct(entity),
                 },
             })
         except ObjectDoesNotExist:
             return JsonResponse(status=HTTPStatus.NOT_FOUND, data={})
-        except ValueError:
+        except (KeyError, ValueError):
             return JsonResponse(status=HTTPStatus.BAD_REQUEST, data={})
         except IntegrityError:
             return JsonResponse(status=HTTPStatus.CONFLICT, data={})
