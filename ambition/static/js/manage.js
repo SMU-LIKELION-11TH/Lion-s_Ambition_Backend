599be7d669afdac7c7533e6e1f12d897418c24b0
Uhyeonj <dngusdlqwkd@gmail.com>
2023-07-12 21:15:46 +0900

    정우현

---
+++ ambition/static/js/manage.js
@@ -0,0 +1,19 @@
+
+function logOutPage() {
+    //로그아웃 로직 구현하세요
+    location.href = "login_view.html";
+}
+function goToOrderPage() {
+    // 주문 페이지로 이동하는 로직을 구현하세요
+    location.href = "order_view.html";
+}
+
+function goToMenuPage() {
+    // 메뉴 페이지로 이동하는 로직을 구현하세요
+    location.href = "menu_view.html";
+}
+
+function goToSalesPage() {
+    // 매출 페이지로 이동하는 로직을 구현하세요
+    location.href = "sales_view.html";
+}
