30e8239691f78912fd935f760efc2446b2a9a6b2
주현지 <hyjoo0909@naver.com>
2023-07-14 01:24:38 +0900

    feat: 페이지 로그인 사용자 이름 표시 기능 구현
    MainView(가게 시작 페이지), OverView(가게 주문 확인 페이지)

---
+++ ambition/store/views/pages.py
@@ -0,0 +1,36 @@
+from django.http import HttpRequest, HttpResponse
+from django.views.generic import  TemplateView
+from django.shortcuts import render
+
+from store.models import *
+
+from store.serializers import *
+
+
+class MainView(TemplateView):
+    template_name='manage_view.html'
+
+    def get(self, request:HttpRequest) -> HttpResponse:
+        """사용자 이름 출력"""
+        print(User.current_user(request))
+        return render(
+            request, 
+            template_name= MainView.template_name, 
+            context={
+            'user': serializeUser(User.current_user(request))
+            }
+        )
+    
+class OrderView(TemplateView):
+    template_name='ordermenu.html'
+
+    def get(self, request:HttpRequest) -> HttpResponse:
+        """사용자 이름 출력"""
+        print(User.current_user(request))
+        return render(
+            request, 
+            template_name= OrderView.template_name, 
+            context={
+            'user': serializeUser(User.current_user(request))
+            }
+        )
