79deab84ea8d66c95b9dacdb3abf6e8bec36073b
Hepheir <hepheir@gmail.com>
2023-07-14 06:15:37 +0900

    refactor: RequireLoginTemplateView로 로그인이 필요한 뷰 정리

--- ambition/store/views/pages.py
+++ ambition/store/views/pages.py
@@ -1,41 +1,26 @@
 from django.http import HttpRequest, HttpResponse
 from django.views.generic import View, TemplateView
-from django.shortcuts import render, redirect
+from django.shortcuts import redirect
 
 from store.models import *
-
 from store.serializers import *
 
 
-class MainView(TemplateView):
-    template_name='manage_view.html'
-
-    def get(self, request:HttpRequest) -> HttpResponse:
-        """사용자 이름 출력"""
-        print(User.current_user(request))
-        return render(
-            request, 
-            template_name= MainView.template_name, 
-            context={
-            'user': serializeUser(User.current_user(request))
-            }
-        )
-    
-class OrderView(TemplateView):
-    template_name='ordermenu.html'
-
-    def get(self, request:HttpRequest) -> HttpResponse:
-        """사용자 이름 출력"""
-        print(User.current_user(request))
-        return render(
-            request, 
-            template_name= OrderView.template_name, 
-            context={
-            'user': serializeUser(User.current_user(request))
-            }
-        )
-    
 class LogoutPageView(View):
     def get(self, request:HttpRequest) -> HttpResponse :
         User.unauthenticate(request)
         return redirect('/login')
+
+
+class RequireLoginTemplateView(TemplateView):
+    """로그인을 필요로 하는 템플릿.
+
+    user 에 사용자 정보를 담아서 준다.
+
+    user.name : 사용자 이름
+    user.email : 사용자 이메일
+    """
+    def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context['user'] = serializeUser(User.current_user(self.request))
+        return context
